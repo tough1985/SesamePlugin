0a4ea1ba4c6844c0e7ea46a298fd902dfb3f8ff6
刘坤 <liukun@fangdd.com>
2018-08-20 19:13:36 +0800

    [NFF]添加Model文件

--- src/main/java/me/xiba/plugin/SesameAction.java
+++ src/main/java/me/xiba/plugin/SesameAction.java
@@ -10,11 +10,26 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.vfs.VirtualFile;
+import me.xiba.plugin.template.ModelTemplate;
+import org.apache.commons.collections.keyvalue.DefaultKeyValue;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SesameAction extends AnAction {
+
+    public static final String KEY_METHOD_NAME = "method_name";
+    public static final String KEY_METHOD_RETURN = "method_return";
+    public static final String KEY_METHOD_PARAMS = "method_params";
+
+
+    String name;
 
     @Override
     public void actionPerformed(AnActionEvent e) {
@@ -39,6 +54,8 @@ public class SesameAction extends AnAction {
         sb.append("threadName=" + Thread.currentThread().getName());
         sb.append("\n");
 
+        name = file.getName().replace("Service.java", "");
+
         try {
             //创建「model」文件夹
             VirtualFile modelDir = parentDir.findChild("model");
@@ -49,19 +66,43 @@ public class SesameAction extends AnAction {
             //获取「model」文件名称
             String modelFileName = file.getName().replace("Service", "Model");
 
-            // 如果ModelXxx.java文件
-            if(modelDir != null && modelDir.exists()) {
-                modelDir.findOrCreateChildData(file.getUserData(VirtualFile.REQUESTOR_MARKER), modelFileName);
-            }
+
 
 
             String modelFileFullName = modelDir.getPath() + "/" + modelFileName;
+            File modelFile = new File(modelFileFullName);
 
-            // 写文件
-            FileWriter fileWriter = new FileWriter(modelFileFullName);
-            fileWriter.write(getSourceText(e));
-            fileWriter.close();
+
+
+            String modelContent;
+            if(modelFile.exists()) {
+                modelContent = generatorModelMethod(parseMethod(getSourceText(e))) + "\n}";
+
+
+
+            } else {
+                // 如果ModelXxx.java文件
+                if(modelDir != null && modelDir.exists()) {
+                    modelDir.findOrCreateChildData(file.getUserData(VirtualFile.REQUESTOR_MARKER), modelFileName);
+                }
+                String packagePath = getPackagePath(e);
+                modelContent = generateModelFile(packagePath, name, generatorModelMethod(parseMethod(getSourceText(e))));
+
+            }
+
+
+            RandomAccessFile randomAccessFile = new RandomAccessFile(modelFileFullName, "rw");
+
+            if (randomAccessFile.length() > 2){
+                randomAccessFile.seek(randomAccessFile.length() - 2);
+            }
+
+            randomAccessFile.write(modelContent.getBytes("UTF-8"));
+
+            randomAccessFile.close();
+
             sb.append("modelFileFullName=" + modelFileFullName);
+
 
 
 
@@ -89,6 +130,7 @@ public class SesameAction extends AnAction {
         if (startLine == -1 || endLine == -1){
             throw new IllegalArgumentException("未能找到可转化的目标文本");
         }
+
 
         String sourceText = getRangeText(document, startLine, endLine);
 
@@ -173,4 +215,150 @@ public class SesameAction extends AnAction {
 
         return document.getText(new TextRange(lineStart, lineEnd));
     }
+
+    /**
+     * 解析接口方法
+     * @param methodStr
+     */
+    private Map<String, Object> parseMethod(String methodStr){
+        if (methodStr == null || methodStr.toString().equals("")){
+            return null;
+        }
+
+        Map<String, Object> paramMap = new HashMap<>();
+
+        StringBuilder sb = new StringBuilder();
+
+
+        // 获取返回值
+        String returnStartStr = "BaseHttpResult<";
+        int indexStartReturn = methodStr.indexOf(returnStartStr);
+        int indexEndReturn = methodStr.indexOf("> ");
+
+        String returnStr = methodStr.substring(indexStartReturn + returnStartStr.length(), indexEndReturn - 1);
+        // 添加返回值
+        paramMap.put(KEY_METHOD_RETURN, returnStr);
+
+        sb.append("returnStr = " + returnStr);
+        sb.append("\n");
+
+        // 获取方法名
+        String methodNameAndParam = methodStr.substring(indexEndReturn + 1);
+        int methodNameEndIndex = methodNameAndParam.indexOf("(");
+
+        String methodNameStr = methodNameAndParam.trim().substring(0, methodNameEndIndex - 1);
+        // 添加方法名
+        paramMap.put(KEY_METHOD_NAME, methodNameStr);
+
+        sb.append("methodNameStr = " + methodNameStr);
+        sb.append("\n");
+
+        // 获取方法参数
+        int methodParamEndIndex = methodNameAndParam.indexOf(");");
+        String methodParamStr = methodNameAndParam.trim().substring(methodNameEndIndex, methodParamEndIndex - 1);
+
+        String[] methodParams = methodParamStr.split(",");
+
+        List<DefaultKeyValue> paramList = new ArrayList<DefaultKeyValue>();
+        if(methodParams.length > 0){
+            for (int i = 0; i < methodParams.length; i++) {
+                String[] paramCons = methodParams[i].trim().split(" ");
+
+                if (paramCons.length > 1){
+                    DefaultKeyValue keyValue = new DefaultKeyValue();
+                    // 参数名做为key，因为参数名不重复
+                    keyValue.setKey(paramCons[2]);
+                    // 参数值做为value
+                    keyValue.setValue(paramCons[1]);
+
+                    paramList.add(keyValue);
+                }
+
+            }
+        }
+
+        // 添加方法参数
+        paramMap.put(KEY_METHOD_PARAMS, paramList);
+
+        return paramMap;
+    }
+
+    /**
+     * 生成Model中的对应方法
+     * @param paramMap
+     * @return
+     */
+    public String generatorModelMethod(Map<String, Object> paramMap){
+        if (paramMap == null) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        sb.append("\tpublic void ");
+        sb.append(paramMap.get(KEY_METHOD_NAME));
+        sb.append("(");
+
+        List<DefaultKeyValue> paramList = (List<DefaultKeyValue>) paramMap.get(KEY_METHOD_PARAMS);
+
+        StringBuilder params = new StringBuilder();
+        if (paramList.size() > 0){
+            DefaultKeyValue defaultKeyValue;
+            for (int i = 0; i < paramList.size(); i++) {
+                defaultKeyValue = paramList.get(i);
+                sb.append(defaultKeyValue.getValue());
+                sb.append(" ");
+                sb.append(defaultKeyValue.getKey());
+                sb.append(", ");
+
+                params.append(defaultKeyValue.getKey());
+                if(i != paramList.size() - 1){
+                    params.append(", ");
+                }
+            }
+        }
+
+        sb.append(String.format("LoadingObserver<%s> loadingObserver", paramMap.get(KEY_METHOD_RETURN)));
+        sb.append(") {\n");
+
+        String methodBody = String.format("\t\tObservable observable = getService().%1$s(%2$s);\n", paramMap.get(KEY_METHOD_NAME), params.toString());
+        sb.append(methodBody);
+        sb.append("\t\tmakeSubscribe(observable, loadingObserver);\n");
+        sb.append("\t}");
+        sb.append("\n");
+
+        return sb.toString();
+    }
+
+    /**
+     * 生成model文件内容
+     * @param packagePath
+     * @param name
+     * @param method
+     */
+    public String generateModelFile(String packagePath, String name, String method){
+
+        String modelFile = String.format(ModelTemplate.MODEL_TEMPLATE, packagePath, name, method);
+
+        return modelFile;
+
+    }
+
+    /**
+     * 获取service文件的包路径
+     * @param e
+     * @return
+     */
+    private String getPackagePath(AnActionEvent e){
+        Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+
+        Document document = editor.getDocument();
+        String firstLineText = getSelectedText(document, 0);
+
+        String startText = "package ";
+        int startIndex = firstLineText.indexOf(startText);
+        int endIndex = firstLineText.indexOf(".service;");
+        return firstLineText.substring(startIndex + startText.length(), endIndex);
+    }
+
 }
---
+++ src/main/java/me/xiba/plugin/template/ModelTemplate.java
@@ -0,0 +1,30 @@
+package me.xiba.plugin.template;
+
+public class ModelTemplate {
+    //第一个参数是包路径
+    //第二个参数是类名
+    //第三个参数是对应的方法体
+    public static String MODEL_TEMPLATE =
+            "package %1$s.model;\n" +
+                    "\n" +
+                    "\n" +
+                    "import com.fangdd.mobile.mvvmcomponent.model.BaseHttpModel;\n" +
+                    "import com.fangdd.mobile.realtor.common.http.LoadingObserver;\n" +
+                    "import %1$s.service.%2$sService;\n" +
+                    "\n" +
+                    "import io.reactivex.Observable;\n" +
+                    "\n" +
+                    "/**\n" +
+                    " * @Author:\n" +
+                    " * @Date:\n" +
+                    " * @Description:\n" +
+                    " */\n" +
+                    "public class %2$sModel extends BaseHttpModel {\n" +
+                    "\n" +
+                    "    public %2$sService get%2$sService() {\n" +
+                    "    }\n" +
+                    "\n" +
+                    "%3$s" +
+                    "\n" +
+                    "}\n";
+}
